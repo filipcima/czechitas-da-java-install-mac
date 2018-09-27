7d99885b693f4539fbf2d33043dcf2a05ea6eb4d
Filip Cima <filip.cima@firma.seznam.cz>
2018-09-27 17:17:45 +0200

    parsing filenames for wget from url in a loop

--- install.sh
+++ install.sh
@@ -5,11 +5,17 @@ else
   brew install wget
 fi
 
-wget -O mysql-8.0.12-macos10.13-x86_64.dmg https://www.dropbox.com/s/7rvr7vwfo1plcmh/mysql-8.0.12-macos10.13-x86_64.dmg?dl=1
-wget -O jdk-8u181-macosx-x64.dmg jdk-8u181-macosx-x64.dmg https://www.dropbox.com/s/vpzytohnwonzs1s/jdk-8u181-macosx-x64.dmg?dl=1
-wget -O mysql-workbench-community-8.0.12-macos-x86_64.dmg https://www.dropbox.com/s/q6mo0otqz0f0jmk/mysql-workbench-community-8.0.12-macos-x86_64.dmg?dl=1
-wget -O eclipse-inst-mac64.tar.gz https://www.dropbox.com/s/1lihg2jsk88x6b8/eclipse-inst-mac64.tar.gz?dl=1
-# wget -O jdk1.8.0.tar.gz https://www.dropbox.com/s/5df0rowx9136acf/jdk1.8.0.tar.gz?dl=1
+links=(
+  "https://www.dropbox.com/s/7rvr7vwfo1plcmh/mysql-8.0.12-macos10.13-x86_64.dmg?dl=1"
+  "https://www.dropbox.com/s/vpzytohnwonzs1s/jdk-8u181-macosx-x64.dmg?dl=1"
+  "https://www.dropbox.com/s/q6mo0otqz0f0jmk/mysql-workbench-community-8.0.12-macos-x86_64.dmg?dl=1"
+  "https://www.dropbox.com/s/1lihg2jsk88x6b8/eclipse-inst-mac64.tar.gz?dl=1"
+  # "https://www.dropbox.com/s/5df0rowx9136acf/jdk1.8.0.tar.gz?dl=1"
+)
+for link in "${links[@]}"
+do
+  wget -O $(echo $link | sed -e "s/?dl=1$//" -e 's/.*\///') $link
+done
 
 for filename in *.tar.gz
 do
