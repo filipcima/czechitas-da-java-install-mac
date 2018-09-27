7c09a43e1678e53156acbd330147a6aa71260f72
Filip Cima <filip.cima@firma.seznam.cz>
2018-09-27 15:20:29 +0200

    initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+if brew ls --versions myformula > /dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install wget
+else
+  brew install wget
+fi
+
+wget -O mysql-8.0.12-macos10.13-x86_64.dmg https://www.dropbox.com/s/7rvr7vwfo1plcmh/mysql-8.0.12-macos10.13-x86_64.dmg?dl=1
+wget -O mysql-8.0.12-macos10.13-x86_64.dmg jdk-8u181-macosx-x64.dmg https://www.dropbox.com/s/vpzytohnwonzs1s/jdk-8u181-macosx-x64.dmg?dl=1
+wget -O mysql-workbench-community-8.0.12-macos-x86_64.dmg https://www.dropbox.com/s/q6mo0otqz0f0jmk/mysql-workbench-community-8.0.12-macos-x86_64.dmg?dl=1
+wget -O eclipse-inst-mac64.tar.gz https://www.dropbox.com/s/1lihg2jsk88x6b8/eclipse-inst-mac64.tar.gz?dl=1
+wget -O jdk1.8.0.tar.gz https://www.dropbox.com/s/5df0rowx9136acf/jdk1.8.0.tar.gz?dl=1
+
+for filename in *.tar.gz
+do
+  tar -xf $filename
+  printf "soubor %s extrahovan!\n" "$filename"
+  rm -f $filename
+  printf "archiv %s odstranen!\n" "$filename"
+done
