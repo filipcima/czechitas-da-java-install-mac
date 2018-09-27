f4a5da413557a9c51e86e883bfc2414fee486331
Filip Cima <filip.cima@firma.seznam.cz>
2018-09-27 15:49:22 +0200

    bad filename fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ else
 fi
 
 wget -O mysql-8.0.12-macos10.13-x86_64.dmg https://www.dropbox.com/s/7rvr7vwfo1plcmh/mysql-8.0.12-macos10.13-x86_64.dmg?dl=1
-wget -O mysql-8.0.12-macos10.13-x86_64.dmg jdk-8u181-macosx-x64.dmg https://www.dropbox.com/s/vpzytohnwonzs1s/jdk-8u181-macosx-x64.dmg?dl=1
+wget -O jdk-8u181-macosx-x64.dmg jdk-8u181-macosx-x64.dmg https://www.dropbox.com/s/vpzytohnwonzs1s/jdk-8u181-macosx-x64.dmg?dl=1
 wget -O mysql-workbench-community-8.0.12-macos-x86_64.dmg https://www.dropbox.com/s/q6mo0otqz0f0jmk/mysql-workbench-community-8.0.12-macos-x86_64.dmg?dl=1
 wget -O eclipse-inst-mac64.tar.gz https://www.dropbox.com/s/1lihg2jsk88x6b8/eclipse-inst-mac64.tar.gz?dl=1
 wget -O jdk1.8.0.tar.gz https://www.dropbox.com/s/5df0rowx9136acf/jdk1.8.0.tar.gz?dl=1
@@ -14,7 +14,7 @@ wget -O jdk1.8.0.tar.gz https://www.dropbox.com/s/5df0rowx9136acf/jdk1.8.0.tar.g
 for filename in *.tar.gz
 do
   tar -xf $filename
-  printf "soubor %s extrahovan!\n" "$filename"
+  printf "Soubor %s extrahovan!\n" "$filename"
   rm -f $filename
-  printf "archiv %s odstranen!\n" "$filename"
+  printf "Archiv %s odstranen!\n" "$filename"
 done
